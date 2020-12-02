4e405a972780ba281e3986effd4f9676ba881267
iLaGMatiVR <matiasrecalde514@gmail.com>
2020-12-02 00:23:49 -0300

    Creacion del menu c/accesos a formularios, y creatividad

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,70 @@ namespace SistemaVentasWPF
         {
             InitializeComponent();
         }
+
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_Usuario.Visibility = Visibility.Collapsed;
+                tt_Consulta.Visibility = Visibility.Collapsed;
+
+
+                tt_signout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_Usuario.Visibility = Visibility.Visible;
+                tt_Consulta.Visibility = Visibility.Visible;
+
+
+                tt_signout.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 1;
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 0.3;
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        private void TextBlock_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Usuarioswpf usuario = new Usuarioswpf();
+
+            //usuario.Show();
+            MainWindow menu = new MainWindow();
+            menu.Hide();
+        }
+
+        private void TextBlock_PreviewMouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            Consultawpf consul = new Consultawpf();
+            consul.Show();
+            MainWindow menu = new MainWindow();
+            menu.Hide();
+        }
     }
+
 }
+
